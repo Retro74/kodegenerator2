7f7502bc10b8fc08e5081362160edac34fa98b9f
Roger S. Mikalsen <rmikalsen@hotmail.com>
2026-04-07 08:59:04 +0200

    tilkobling

--- js/gen1.js
+++ js/gen1.js
@@ -1,51 +1,59 @@
 // gen1 widget object
-var gen1 = (function () {
+var gen1 = (function() {
 
     // settings
     var s = {
-        user: $('#gen1User'),
-        pass: $('#gen1Pass'),
-        db: $('#gen1Db'),
+        server: $('#gen1Server'),
+        user:   $('#gen1User'),
+        pass:   $('#gen1Pass'),
+        db:     $('#gen1Db'),
         submit: $('#gen1Button'),
         output: $('#gen1Out')
     };
 
+    // self object
     var self = {};
 
     // bindings
-    self.setupBindings = function () {
-        s.submit.on('click', function () {
+    self.setupBindings = function() {
+        s.submit.on('click', function() {
             self.generate();
         });
     };
 
     // handlers
-    self.generate = function () {
-
-        var user = s.user.val().trim();
-        var pass = s.pass.val().trim();
-        var db   = s.db.val().trim();
-
+    self.generate = function() {
         var code = '';
-
         code += '&lt;?php\n';
-        code += '<div class="phpComment">// Databasetilkobling med server, brukernavn, passord og databasenavn</div>\n';
-        code += '$tilkobling = new mysqli("localhost", "' + user + '", "' + pass + '", "' + db + '");\n\n';
-        code += '<div class="phpComment">// Sjekk at tilkoblingen var vellykket</div>\n';
-        code += 'if ($tilkobling->connect_error) {\n';
-        code += '    die("Tilkoblingsfeil: " . $tilkobling->connect_error);\n';
-        code += '}\n';
+        code += '<div class="phpComment">// Databasetilkobling – oppgitt server, brukernavn, passord og databasenavn.</div>';
+        code += '    $tilkobling = new mysqli("' + s.server.val() + '", "' + s.user.val() + '", "' + s.pass.val() + '", "' + s.db.val() + '");\n';
+        code += '<div class="phpComment">// SIKKERHET – passord i produksjon:</div>';
+        code += '<div class="phpComment">// Ikke bruk standardpassordet til root i produksjon.</div>';
+        code += '<div class="phpComment">// Sett eget <strong>!langt!</strong> passord  i phpMyAdmin: Brukere → root → Endre passord,</div>';
+        code += '<div class="phpComment">// eller SQL-kommandoen: ALTER USER \'root\'@\'localhost\' IDENTIFIED BY \'NyttPassord\';</div>\n';
+        code += '<div class="phpComment">// ADVARSEL - Denne filen inneholder brukernavn og passord i klartekst</div>';
+        code += '<div class="phpComment">// Ikke publiser den til et sted hvor innholdet kan leses.</div>';
+        code += '<div class="phpComment">// Tips: Legg til filens navn f.eks. "db_connect.php" i ".gitignore".</div>';
+        code += '\n';
+        code += '<div class="phpComment">// Sjekk om tilkoblingen feilet:</div>';
+        code += '    if ($tilkobling->connect_error) {\n';
+        code += '        error_log("Tilkoblingsfeil: " . $tilkobling->connect_error);\n';
+        code += '        die("Kunne ikke koble til databasen.");\n';
+        code += '    }\n';
         code += '?&gt;';
 
-        // Output
+        // clearing output
         s.output.html('');
+
+        // adding output
         s.output.html(code);
     };
 
     // init
-    self.init = function () {
+    self.init = function() {
         self.setupBindings();
     };
 
+    // return self
     return self;
 }());
